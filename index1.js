f8e05359d4013b6eae0b8773603f78d705ea7f60
BarakaIsaac <isaac.mwandau2@gmail.com>
2022-12-16 17:34:28 +0300

    Completed Assigment

---
+++ index1.js
@@ -0,0 +1,79 @@
+fetch('https://my-json-server.typicode.com/wanjirumuigai/flatabeer/beers').then((response) => {
+    console.log('resolved', response);
+    return response.json();
+}).then(data => {
+    //get details the first beer from server
+
+    document.getElementById("beer-name").textContent = data[0].name;
+    document.getElementById("beer-image").src = data[0].image_url
+    document.getElementById("beer-description").textContent = data[0].description
+        //add a list of beers from the server
+    document.getElementById("beer-list").innerHTML = "";
+    data.forEach(user => {
+
+        const markup = `<li>${user.name}</li>`
+        document.querySelector("#beer-list").insertAdjacentHTML('beforeend', markup)
+    });
+
+    //replace reviews    
+    const serverReviews = data[0].reviews // get reviews array from server 
+    document.getElementById("review-list").innerHTML = ""; //delete the sample reviews from the html doc
+    for (let i = 0; i < serverReviews.length; i++) { //loop through the reviews array and add the reviews to the html doc
+        const markup = `<li>${serverReviews[i]}</li>`
+
+        document.querySelector("#review-list").insertAdjacentHTML('beforeend', markup)
+
+    }
+
+    //error handling  
+}).catch((err) => {
+    console.log('rejected', err);
+});
+
+function handleData() {
+
+}
+
+//update description
+const description = document.getElementById('beer-description')
+const formDescription = document.getElementById('description-form')
+
+formDescription.addEventListener('submit', e => {
+        e.preventDefault(); //prevents page reloading
+
+        const newDescr = document.getElementById('description').value.trim()
+
+        if (newDescr.length) { //prevent null or spaces
+            description.innerHTML = newDescr; //replace the beer description with the updated description
+            formDescription.reset();
+        }
+
+    }
+
+);
+
+
+//add review
+const form = document.querySelector("#review-form")
+const review = document.querySelector('#review');
+const list = document.querySelector("#review-list")
+
+const addContent = addReview => {
+    const html = `<li>${addReview}</li>`;
+    list.innerHTML += html
+}
+form.addEventListener('submit', e => {
+    e.preventDefault(); //prevents page reloading
+    const toAdd = review.value.trim() //trims off empty space
+    if (toAdd.length) { //ensure there at least one character typed
+        addContent(toAdd);
+        form.reset()
+    }
+
+});
+
+//delete reviews
+
+list.addEventListener('click', function(e) {
+    this.removeChild(e.target);
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,84 @@
 // Code here
+
+// Define server URL 
+let pathURL = `http://localhost:3000/beers`
+
+// Fetch first beer from server
+fetchFirstBeer();
+
+function fetchFirstBeer(index = 1) {
+    fetch(`${pathURL}/${index}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const beerName = document.getElementById("beer-name")
+            beerName.textContent = data.name;
+            beerName.dataset.id = data.id;
+
+
+            document.getElementById("beer-image").src = data.image_url
+            document.getElementById("beer-description").textContent = data.description
+
+            // reviews
+            const review = data.reviews
+            document.getElementById("review-list").innerHTML = '';
+            for (let i = 0; i < review.length; i++) {
+                const listReviews = `<li>${review[i]}</li>`;
+
+                document.querySelector('#review-list').insertAdjacentHTML('beforeend', listReviews);
+            }
+        })
+
+}
+
+
+// Define instance to fetch all beers from server    
+
+// Fetch all beers from server and replace
+fetchAllBeers();
+
+function fetchAllBeers() {
+    fetch(pathURL)
+        .then((response) => {
+            return response.json();
+        })
+        .then(data => {
+            const listBeers = document.getElementById("beer-list");
+            listBeers.innerHTML = "";
+            data.forEach(element => {
+                const listItem = document.createElement("li");
+                listItem.textContent = element.name;
+                listBeers.appendChild(listItem);
+            })
+        })
+}
+
+// Load Review
+
+document.getElementById("review-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const updatedReviews = [];
+    document.querySelectorAll('#review-list li').forEach(element => {
+        updatedReviews.push(element.textContent);
+    })
+    updatedReviews.push(event.target.review.value);
+    // console.log(reviews);
+    const beerId = document.getElementById("beer-name").dataset.id;
+    fetch(`${pathURL}/${beerId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            body: JSON.stringify({
+                reviews: updatedReviews
+            })
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then(beer => beer)
+        .catch(error => { console.log(error.message); })
+})
